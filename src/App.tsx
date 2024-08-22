d617f16fe84eecb8e972fe64c3d367225146d913
smiles00714 <louisrobinson246@gmail.com>
2024-08-23 01:56:47 +0900

    remove lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 // import { Suspense, lazy, useEffect } from "react";
-import { lazy, useEffect } from "react";
+import { useEffect } from "react";
 
 /** Vendors */
 import { Provider as ReduxProvider } from "react-redux";
 import { ConfigProvider, Layout } from "antd";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CountDetails from "./components/countdown/views/Details";
+import CountList from "./components/countdown/views/List";
+import CreateEvent from "./components/event/views/Add";
+import Day from "./components/week/views/Day";
+import EventDetails from "./components/event/views/Details";
+import EventList from "./components/event/views/List";
+import Navbar from "./components/shared/navbar/Navbar";
+import Week from "./components/week/views/Week";
 import "./dist/css/app.css";
 
 /** Redux */
@@ -12,14 +20,14 @@ import store from "./redux/configureStore";
 
 /** Custom Components */
 import Building from "./components/building/views/Building";
-const CountDetails = lazy(() => import("./components/countdown/views/Details"));
-const CountList = lazy(() => import("./components/countdown/views/List"));
-const CreateEvent = lazy(() => import("./components/event/views/Add"));
-const Day = lazy(() => import("./components/week/views/Day"));
-const EventDetails = lazy(() => import("./components/event/views/Details"));
-const EventList = lazy(() => import("./components/event/views/List"));
-const Navbar = lazy(() => import("./components/shared/navbar/Navbar"));
-const Week = lazy(() => import("./components/week/views/Week"));
+// const CountDetails = lazy(() => import("./components/countdown/views/Details"));
+// const CountList = lazy(() => import("./components/countdown/views/List"));
+// const CreateEvent = lazy(() => import("./components/event/views/Add"));
+// const Day = lazy(() => import("./components/week/views/Day"));
+// const EventDetails = lazy(() => import("./components/event/views/Details"));
+// const EventList = lazy(() => import("./components/event/views/List"));
+// const Navbar = lazy(() => import("./components/shared/navbar/Navbar"));
+// const Week = lazy(() => import("./components/week/views/Week"));
 
 const antdTheme = {
   components: {
